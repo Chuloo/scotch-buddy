37ace1775514d352febab7b69b53e27554bb3df0
home <home@Chuloos-Macbook-Pro.local>
2019-08-07 20:55:19 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        marginTop: "10%"
+      }}
+    >
+      <h1 className="is-size-2">Simple React Webpage</h1>
+      <h2 className="is-size-5">Webpage deployed to Buddy!</h2>
     </div>
   );
 }
